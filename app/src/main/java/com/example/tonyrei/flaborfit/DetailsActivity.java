1a5fd5fcb2e316b81aee7ffdaa4c47fbb71e5f4f
BionicSwash <bionicswash@gmail.com>
2016-06-27 21:17:41 +0100

    first commit

--- app/src/main/java/com/example/tonyrei/flaborfit/DetailsActivity.java
+++ app/src/main/java/com/example/tonyrei/flaborfit/DetailsActivity.java
@@ -2,6 +2,8 @@ package com.example.tonyrei.flaborfit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        TextView excerciseTitle = (TextView)findViewById(R.id.exerciseTitle);
+        ImageView excerciseImage =(ImageView)findViewById(R.id.excerciseImage);
+
+
     }
 }
